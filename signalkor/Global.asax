29441e7c3ff67c8e1c389d85d8e516a01c4b779e
Graeme Christie <graeme@sx.net.au>
2012-05-05 14:26:49 +0800

    Initial Commit

---
+++ signalkor/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="signalkor.MvcApplication" Language="C#" %>
---
+++ signalkor/Hubs/GameHub.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SignalR.Hubs;
+
+namespace signalkor.Hubs
+{
+    public class GameHub : Hub
+    {
+        static List<PlayerInfo> PlayerPositions = new List<PlayerInfo>();
+
+        public void MoveTo(string player, int x, int y)
+        {
+            var playerPosition = PlayerPositions.FirstOrDefault(pp=>pp.player == player);
+            if (playerPosition == null)
+            {
+                PlayerPositions.Add(new PlayerInfo { player=player, x = x, y = y });
+            } else {
+                playerPosition.x = x;
+                playerPosition.y = y;
+            }
+            Clients.updatePlayerPositions(PlayerPositions);
+        }
+
+        public void GetPlayerPositions()
+        {
+            Clients.updatePlayerPositions(PlayerPositions);
+        }
+    }
+
+    public class PlayerInfo
+    {
+        public string player { get; set; }
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+}
