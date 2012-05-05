7c132f9431ac60632ad8c464d5f0003cf98db5bd
Graeme Christie <graeme@sx.net.au>
2012-05-05 22:27:07 +0800

    Updated to use draggable

---
+++ signalkor/Scripts/knockout.extensions.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="jquery-1.6.4-vsdoc.js" />
+/// <reference path="knockout.debug.js" />
+/// <reference path="jquery-ui-1.8.19.js" />
+
+
+ko.bindingHandlers.draggable = {
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+        var value = ko.utils.unwrapObservable(valueAccessor());
+        $(element).draggable(value);
+    }
+};
